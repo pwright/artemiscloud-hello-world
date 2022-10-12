a6a27205675d5a68ae68793756da0bbe1f1682d3
Paul Wright <pwright@redhat.com>
2022-10-12 12:46:24 +0100

    git subrepo clone --branch=deploy_scripts https://github.com/brusdev/activemq-artemis-operator subrepos/operator
    subrepo:
      subdir:   "subrepos/operator"
      merged:   "bfcbb026"
    upstream:
      origin:   "https://github.com/brusdev/activemq-artemis-operator"
      branch:   "deploy_scripts"
      commit:   "bfcbb026"
    git-subrepo:
      version:  "0.4.5"
      origin:   "???"
      commit:   "???"

---
+++ subrepos/operator/subrepos/operator/pkg/resources/pods/pod.go
@@ -0,0 +1,29 @@
+package pods
+
+import (
+	"github.com/artemiscloud/activemq-artemis-operator/pkg/utils/namer"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	logf "sigs.k8s.io/controller-runtime"
+)
+
+var log = logf.Log.WithName("package pods")
+var NameBuilder namer.NamerData
+
+func MakePodTemplateSpec(current *corev1.PodTemplateSpec, namespacedName types.NamespacedName, labels map[string]string) *corev1.PodTemplateSpec {
+
+	var desired *corev1.PodTemplateSpec = current
+	if desired == nil {
+		desired = &corev1.PodTemplateSpec{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      namespacedName.Name,
+				Namespace: namespacedName.Namespace,
+			},
+			Spec: corev1.PodSpec{},
+		}
+	}
+	desired.ObjectMeta.Labels = labels
+
+	return desired
+}
