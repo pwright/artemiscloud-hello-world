232e0ad58e69ec035f6e1421825a15189cce7125
Paul Wright <pwright@redhat.com>
2022-10-12 12:56:02 +0100

    git subrepo clone --branch=deploy_scripts https://github.com/brusdev/activemq-artemis-operator subrepos/operator
    subrepo:
      subdir:   "subrepos/operator"
      merged:   "cbe2e7ec"
    upstream:
      origin:   "https://github.com/brusdev/activemq-artemis-operator"
      branch:   "deploy_scripts"
      commit:   "cbe2e7ec"
    git-subrepo:
      version:  "0.4.5"
      origin:   "???"
      commit:   "???"

---
+++ subrepos/operator/pkg/resources/statefulsets/statefulset.go
@@ -0,0 +1,81 @@
+package statefulsets
+
+import (
+	"context"
+
+	"github.com/artemiscloud/activemq-artemis-operator/pkg/resources/pods"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/types"
+	logf "sigs.k8s.io/controller-runtime"
+)
+
+var log = logf.Log.WithName("package statefulsets")
+
+func MakeStatefulSet2(currentStateFulSet *appsv1.StatefulSet, ssName string, svcHeadlessName string, namespacedName types.NamespacedName, annotations map[string]string, labels map[string]string, replicas int32) *appsv1.StatefulSet {
+
+	if currentStateFulSet == nil {
+		currentStateFulSet = &appsv1.StatefulSet{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "StatefulSet",
+				APIVersion: "apps/v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      ssName,
+				Namespace: namespacedName.Namespace,
+			},
+			Spec: appsv1.StatefulSetSpec{
+				// these fields are immutable
+				ServiceName: svcHeadlessName,
+				Selector: &metav1.LabelSelector{
+					MatchLabels: labels,
+				},
+			},
+		}
+	}
+	// these fields we reconcile
+	currentStateFulSet.ObjectMeta.Labels = labels
+	currentStateFulSet.ObjectMeta.Annotations = annotations
+
+	currentStateFulSet.Spec.Replicas = &replicas
+	currentStateFulSet.Spec.Template = *pods.MakePodTemplateSpec(&currentStateFulSet.Spec.Template, namespacedName, labels)
+
+	return currentStateFulSet
+}
+
+var GLOBAL_CRNAME string = ""
+
+func RetrieveStatefulSet(statefulsetName string, namespacedName types.NamespacedName, labels map[string]string, client client.Client) (*appsv1.StatefulSet, error) {
+
+	// Log where we are and what we're doing
+	reqLogger := log.WithValues("ActiveMQArtemis Name", namespacedName.Name)
+	reqLogger.Info("Retrieving " + "StatefulSet " + statefulsetName)
+
+	var err error = nil
+
+	ss := &appsv1.StatefulSet{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "StatefulSet",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        statefulsetName,
+			Namespace:   namespacedName.Namespace,
+			Labels:      labels,
+			Annotations: nil,
+		},
+	}
+
+	if err = client.Get(context.TODO(), namespacedName, ss); err != nil {
+		if errors.IsNotFound(err) {
+			reqLogger.V(1).Info("StatefulSet claim IsNotFound", "Namespace", namespacedName.Namespace, "Name", namespacedName.Name)
+		} else {
+			reqLogger.V(1).Info("StatefulSet claim found", "Namespace", namespacedName.Namespace, "Name", namespacedName.Name)
+		}
+	}
+
+	return ss, err
+}
---
+++ subrepos/operator/pkg/utils/common/auto_detect.go
@@ -0,0 +1,71 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package common
+
+import (
+	"k8s.io/client-go/discovery"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+)
+
+type AutoDetector struct {
+	dc discovery.DiscoveryInterface
+}
+
+// New creates a new auto-detect runner
+func NewAutoDetect(mgr manager.Manager) (*AutoDetector, error) {
+	dc, err := discovery.NewDiscoveryClientForConfig(mgr.GetConfig())
+	if err != nil {
+		return nil, err
+	}
+
+	return &AutoDetector{dc: dc}, nil
+}
+
+func (b *AutoDetector) DetectOpenshift() error {
+	apiGroupVersion := "operator.openshift.io/v1"
+	kind := OpenShiftAPIServerKind
+	stateManager := GetStateManager()
+	isOpenshift, err := ResourceExists(b.dc, apiGroupVersion, kind)
+
+	if err != nil {
+		return err
+	}
+
+	if isOpenshift {
+		// Set state that its Openshift (helps to differentiate between openshift and kubernetes)
+		stateManager.SetState(OpenShiftAPIServerKind, true)
+	} else {
+		stateManager.SetState(OpenShiftAPIServerKind, false)
+	}
+	return nil
+}
+
+func ResourceExists(dc discovery.DiscoveryInterface, apiGroupVersion, kind string) (bool, error) {
+	_, apiLists, err := dc.ServerGroupsAndResources()
+	if err != nil {
+		return false, err
+	}
+	for _, apiList := range apiLists {
+		if apiList.GroupVersion == apiGroupVersion {
+			for _, r := range apiList.APIResources {
+				if r.Kind == kind {
+					return true, nil
+				}
+			}
+		}
+	}
+	return false, nil
+}
